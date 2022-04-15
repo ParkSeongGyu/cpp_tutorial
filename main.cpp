733c93c8978cfe81a24b0dbfc32954538020a4f2
Park Seong Gyu <bluelomu@gmail.com>
2022-04-15 21:02:05 +0900

    main: Using mylib as library
    Signed-off-by: Park Seong Gyu <bluelomu@gmail.com>

--- main.cpp
+++ main.cpp
@@ -1,10 +1,14 @@
 #include <iostream>
+#include "mylib/mylib.h"
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello world" << endl;
+    calculator* calc = getCalculator();
+    int ret = calc->sum(5, 2);
+
+    cout << ret << endl;
 
     return 0;
 }
